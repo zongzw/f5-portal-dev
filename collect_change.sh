d7be0c27542c3d85a10184322531f08d89129900
Andrew Zong <a.zong@f5.com>
2019-08-16 09:26:07 +0800

    add collect_change.sh

---
+++ collect_change.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+source $(dirname $0)/cdir.rc
+
+if [ $# -ne 2 ]; then echo "$0 co1 co2"; exit 1; fi
+
+
+(
+    cd $cdir
+    
+    filelist=`git diff $1 $2 | grep -E "^\+\+\+ b/horizon/" | grep -oE "horizon/.*$"`
+
+    mkdir -p dist 
+    rm -rf dist/*
+    for n in $filelist; do 
+        echo $n;
+        d=`echo $n | sed 's/horizon/dist/'`
+        echo $d
+        mkdir -p $(dirname $d)
+        cp $n $d
+    done
+)
